00590dbae138080383b0003805e9c2e750b88acb
Rahul Shial <rahul.shial@gmail.com>
2022-09-19 09:38:42 -0600

    Header updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Rahul Shial</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Software Developer</h5>
         <CTA />
         <Socials />
 
