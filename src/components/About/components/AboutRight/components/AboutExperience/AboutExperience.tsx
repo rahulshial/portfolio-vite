3c48fede9485c63cdb7da802cf27870a628c3fd8
Rahul Shial <rahul.shial@gmail.com>
2022-09-21 12:28:30 -0600

    About Me content

--- src/components/About/components/AboutRight/components/AboutExperience/AboutExperience.tsx
+++ src/components/About/components/AboutRight/components/AboutExperience/AboutExperience.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './AboutExperience.css';
+import { EXPERIENCE } from './constants';
 
 const AboutExperience = () => {
+
+  const experienceBar = () => {
+    return EXPERIENCE.map((item) => {
+      return (
+        <div key={item.label}className="inner__bar">
+          <div className="bar__fill" style={{width: item.value + '%'}}>
+            <div className="item__label">
+              {item.label}
+            </div>
+          </div>
+          <div className='item__value' style={{width: 100 - item.value + '%'}}>{item.value + '%'}</div>
+        </div>
+      )
+    })
+  }
+    
   return (
-    <div>AboutExperience</div>
-  )
+    <>
+      <div className="bar__container">
+        {experienceBar()}
+      </div>
+    </>
+    )
 }
 
 export default AboutExperience
---
+++ src/components/About/components/AboutRight/components/AboutExperience/constants.ts
@@ -0,0 +1,11 @@
+export const EXPERIENCE = [
+  {label: 'CSS', value: 85},
+  {label: 'HTML', value: 85},
+  {label: 'ReactJS', value: 80},
+  {label: 'Redux', value: 75},
+  {label: 'TypeScript', value: 90},
+  {label: 'NodeJS', value: 65},
+  {label: 'Postgres', value: 75},
+  {label: 'TypeORM', value: 65},
+  {label: 'NestJS', value: 60},
+]
