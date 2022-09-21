67cd349cf9befaff4f51344900607c587b8fd83b
Rahul Shial <rahul.shial@gmail.com>
2022-09-21 08:02:04 -0600

    Change background image, increase fontsize for socials

--- src/components/About/components/AboutRight/AboutRight.tsx
+++ src/components/About/components/AboutRight/AboutRight.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './AboutRight.css';
 import AboutCards from './components/AboutCards/AboutCards';
+import AboutExperience from './components/AboutExperience/AboutExperience';
 
 const AboutRight = () => {
   return (
     <div className="about__content">
       <AboutCards />
-
+      <AboutExperience />
     </div>
   )
 }
---
+++ src/components/About/components/AboutRight/components/AboutExperience/AboutExperience.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutExperience = () => {
+  return (
+    <div>AboutExperience</div>
+  )
+}
+
+export default AboutExperience
